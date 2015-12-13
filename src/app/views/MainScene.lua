c496724afd9b620391fec04721e9e434738d1731
Xintao Yang <xintaoyang@XintaodeMacBook-Pro.local>
2015-12-13 23:19:00 +0800

    Initial Commit

---
+++ src/app/views/MainScene.lua
@@ -0,0 +1,27 @@
+
+local MainScene = class("MainScene", cc.load("mvc").ViewBase)
+
+function MainScene:onCreate()
+    -- add background image
+    display.newSprite("HelloWorld.png")
+        :move(display.center)
+        :addTo(self)
+
+    -- add HelloWorld label
+    cc.Label:createWithSystemFont("Hello World", "Arial", 40)
+        :move(display.cx, display.cy + 200)
+        :addTo(self)
+
+    --添加菜单项
+    local nextButton=cc.MenuItemFont:create("Next")
+    :onClicked(function()
+       self:getApp():enterScene("NextScene");
+        end
+        )
+    cc.Menu:create(nextButton)
+    :move(display.cx,display.cy-200)
+    :addTo(self)
+    
+end
+
+return MainScene
